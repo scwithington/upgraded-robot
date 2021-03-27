fbfdc3ba32669cdfb44dbf7ae6bbcf50d31883e2
Cal Withington <scwithington@gmail.com>
2021-03-27 18:38:53 -0400

    set variables for current date and time and print to page, working on local storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-var rowContainerEl = document.querySelectorAll('.row-container');
+var todaysDate = moment().format('dddd, MMMM D, YYYY');
+var currentTime = moment().format('hh:mm:ss');
+
+var myHours = [
+    '09',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+];
+
+$('#currentDay').text(todaysDate);
+
+console.log(currentTime, todaysDate);
+
+setInterval(function() {
+    $('#whatTimeIsIt').text(currentTime);
+}, 1000);
+
+$(document).on('click', 'button', function () {
+    var saveText = (this).attr();
+    var saveHour = (this).attr('');
+
+    localStorage.setItem(saveHour, saveText);
+}
+)
