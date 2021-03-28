05d8d45d8995220fd40ad78cd10e8e53cff40a30
Cal Withington <scwithington@gmail.com>
2021-03-27 21:01:22 -0400

    storing data but hour and text not matching up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,24 @@
-var todaysDate = moment().format('dddd, MMMM D, YYYY');
-var currentTime = moment().format('hh:mm:ss');
+    var todaysDate = moment().format('dddd, MMMM D, YYYY');
+    var currentTime = moment().format('hh:mm');
 
-var myHours = [
+    // console.log(currentTime, todaysDate);
+
+
+    $('#currentDay').text(todaysDate);
+
+
+    // creates function allowing for dynamic clock
+    setInterval(function() {
+        // creating new date and using local time function
+    var num = (new Date).toLocaleTimeString();
+        // changing text to match in HTML
+    $('#whatTimeIsIt').text(num);
+    }, 1000);
+
+
+
+function init() {
+  var myHours = [
     '09',
     '10',
     '11',
@@ -11,20 +28,31 @@ var myHours = [
     '15',
     '16',
     '17',
-];
+  ];
+  var textAreaEl = $("<textarea class='localText'>");
+  for (var i = 0; i < myHours.length; i++){
+    // setting attributes for textarea
+    textAreaEl.attr(myHours[i]);
+    
+    // if text has been saved, show upon page reset
+    if(localStorage.getItem(myHours[i] != null)) {
+        textAreaEl.text(localStorage.getItem(myHours[i]));
+    }
 
-$('#currentDay').text(todaysDate);
+    var saveButton = $("<button class = 'saveBtn col-1'></button>");
+    saveButton.attr(myHours[i]);
 
-console.log(currentTime, todaysDate);
 
-setInterval(function() {
-    $('#whatTimeIsIt').text(currentTime);
-}, 1000);
+    }
 
-$(document).on('click', 'button', function () {
-    var saveText = (this).attr();
-    var saveHour = (this).attr('');
+    // storing text in local storage
+  $(document).on('click', '.saveBtn', function (event) {
+    var saveText = $(this).siblings('.localText').val();
+    var saveHour = $(this).attr('myHours[i]');
 
     localStorage.setItem(saveHour, saveText);
-}
-)
+    console.log(saveHour, saveText);
+  });
+};
+
+init();
